101e749968967f5e6de8ae562c6037fedc925122
sowndv02 <daoson03112002@gmail.com>
2023-10-30 16:45:01 +0700

    first commit

---
+++ src/test/java/sowndv02/learning/security/learning/spring/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sowndv02.learning.security.learning.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
